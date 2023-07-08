eb9f8bfd0090da2132fe710028b4f038c497c5b7
morz <morz21st@gmail.com>
2023-07-08 18:31:54 +0300

    Added timer when recycling

--- Assets/Scripts/ItemHandlers/SandRecycler.cs
+++ Assets/Scripts/ItemHandlers/SandRecycler.cs
@@ -1,22 +1,29 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SandRecycler : MonoBehaviour, IStorage, IDispenser
 {
+	public bool HasFreeSpace => _sandForRecycling.Count < _sandCount;
+
 	[SerializeField] private Currency _currencyPrefab;
 	[SerializeField] private int _sandCount;
 
+	[SerializeField] private float _recycleTime;
+	[SerializeField] private Image _progressImage;
+
 	private Queue<Sand> _sandForRecycling;
-
 	private List<Currency> _currencyStorage;
-
-	public bool HasFreeSpace => _sandForRecycling.Count < _sandCount;
+	private Coroutine _recycleCoroutine;
 
 	private void Start()
 	{
 		_sandForRecycling = new Queue<Sand>();
 		_currencyStorage = new List<Currency>();
+
+		_progressImage.fillAmount = 0;
+		_progressImage.transform.localRotation = Camera.main.transform.localRotation;
 	}
 
 	public void PlaceItem(Item item)
@@ -28,7 +35,7 @@ public class SandRecycler : MonoBehaviour, IStorage, IDispenser
 
 	public bool TryDispensingInStorage(out Item item, IStorage storage)
 	{
-		for (int i = 0; i < _currencyStorage.Count; i ++)
+		for (int i = 0; i < _currencyStorage.Count; i++)
 		{
 			if (storage.TryStore(_currencyStorage[i]))
 			{
@@ -47,22 +54,40 @@ public class SandRecycler : MonoBehaviour, IStorage, IDispenser
 		if (item is Sand sand)
 		{
 			_sandForRecycling.Enqueue(sand);
-			StartRecycle();
+			TryStartRecycle();
 			return true;
 		}
 		else
 			return false;
 	}
 
-	private void StartRecycle()
+	private void TryStartRecycle()
 	{
-		if (_sandForRecycling.Count == 0) return;
+		if (_sandForRecycling.Count == 0 || _recycleCoroutine != null) return;
+		_recycleCoroutine = StartCoroutine(RecycleItem());
+	}
 
-		var sand = _sandForRecycling.Dequeue();
-		Destroy(sand);
+	private IEnumerator RecycleItem()
+	{
+		while (_sandForRecycling.Count > 0)
+		{
+			float remainingTime = 0;
 
-		var currency = Instantiate(_currencyPrefab, transform);
+			var sand = _sandForRecycling.Dequeue();
+			Destroy(sand.gameObject);
 
-		_currencyStorage.Add(currency);
+			while (remainingTime < _recycleTime)
+			{
+				remainingTime += Time.deltaTime;
+				_progressImage.fillAmount = remainingTime / _recycleTime;
+				yield return null;
+			}
+
+			_progressImage.fillAmount = 0;
+			var currency = Instantiate(_currencyPrefab, transform);
+			_currencyStorage.Add(currency);
+		}
+
+		_recycleCoroutine = null;
 	}
 }
