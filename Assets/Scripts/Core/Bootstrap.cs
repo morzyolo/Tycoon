836a3a0fd17a4d7163583f8eba9f35c7fa0e4d04
morz <morz21st@gmail.com>
2023-07-08 15:20:29 +0300

    Added small changes

--- Assets/Scripts/Core/Bootstrap.cs
+++ Assets/Scripts/Core/Bootstrap.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Bootstrap : MonoBehaviour
 {
 	[SerializeField] private PlayerConfig _playerConfig;
-
+	[SerializeField] private List<CurrencyData> _currenciesData;
+	[Space(15)]
 	[SerializeField] private CurrencyObserver _currencyObserver;
 	[SerializeField] private FactoryProgress _factoryProgress;
 	[SerializeField] private TargetTracking _camera;
@@ -20,7 +22,7 @@ public class Bootstrap : MonoBehaviour
 
 		var currencyWalletFactory = new CurrencyWalletFactory(_fileHandler);
 		var currencyWallet = currencyWalletFactory.Create();
-		_currencyObserver.Initialize(currencyWallet);
+		_currencyObserver.Initialize(currencyWallet, _currenciesData);
 		var playerFactory = new PlayerFactory(_updater, _playerConfig, currencyWallet);
 		var player = playerFactory.Create();
 		_input.Initialize(player);
--- Assets/Scripts/CurrencyStorage/CurrencyType.cs
+++ Assets/Scripts/CurrencyStorage/CurrencyType.cs
@@ -1,11 +1,11 @@
 public enum CurrencyType
 {
-	Coin,
-	Banknote,
-	Cluster,
-	Particle,
-	Piece,
-	Shard,
-	Chip,
-	Fragment
+	Coin = 0,
+	Banknote = 1,
+	Particle = 2,
+	Piece = 3,
+	Shard = 4,
+	Chip = 5,
+	Fragment = 6,
+	Cluster = 7
 }
